821c2c488fb4fe966ce98bd33fa31b8d55eb5d4f
Daniel Vilas <daniel.vilas@gmail.com>
2014-01-09 19:27:22 +0100

    Cambiado para el circuito actual

--- ArbolNavidad/ArbolNavidad.ino
+++ ArbolNavidad/ArbolNavidad.ino
@@ -10,13 +10,16 @@
 //          : to count from 0 to 255                           
 //****************************************************************
 
-Chal74hc595 reg = Chal74hc595(1,2);
+Chal74hc595 reg = Chal74hc595(1,1);
 
-
+int sh_cp=8;
+int st_cp=9;
+int ds1 =10;
+int ds2=11;
 
 void setup() {
   //set pins to output so you can control the shift register
-  reg.setup  (11,12,8);
+  reg.setup  (ds1,sh_cp,st_cp);
 }
 
 void loop() {
@@ -35,7 +38,7 @@ void loop() {
     // pause before next value:
     delay(500);
   }*/
-  int a[]={0xFF,0xFF};
+ /* int a[]={0xFF,0xFF};
   reg.shift(a);
   delay(500);
   a[0]=0b10101010;
@@ -63,8 +66,22 @@ void loop() {
     a[0]=a[0]>>1;
     a[1]=a[1]>>1;
     if(i==7)a[0]=0x80;
-
     delay(250);
   }
+  delay(250);
+  */
+  int a[]={0xff};
+  reg.shift(a);
+   delay(500);
+  a[0]=0b10101010;
+   reg.shift(a);
+   a[0]=1;
+   delay(500);
+   for(int i=0;i<8;i++){
+    reg.shift(a);
+    a[0]=a[0]<<1;
+    delay(250);
+  }
+  reg.shift(a);
   delay(250);
 }
