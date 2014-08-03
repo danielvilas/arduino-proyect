afb723eda0f2690f8e8ca6975ebf9600f7c8c43b
Daniel Vilas <daniel.vilas@gmail.com>
2014-08-03 18:30:25 +0200

    Comienzo soporte a varios canales

--- ArbolNavidad/ArbolNavidad.ino
+++ ArbolNavidad/ArbolNavidad.ino
@@ -10,16 +10,17 @@
 //          : to count from 0 to 255                           
 //****************************************************************
 
-Chal74hc595 reg = Chal74hc595(1,1);
+Chal74hc595 reg = Chal74hc595(2,1);
 
 int sh_cp=8;
 int st_cp=9;
 int ds1 =10;
 int ds2=11;
+int ds[]={ds1,ds2};
 
 void setup() {
   //set pins to output so you can control the shift register
-  reg.setup  (ds1,sh_cp,st_cp);
+  reg.setup  (ds,sh_cp,st_cp);
 }
 
 void loop() {
@@ -70,16 +71,19 @@ void loop() {
   }
   delay(250);
   */
-  int a[]={0xff};
+  int a[]={0xff,0xff};
   reg.shift(a);
    delay(500);
-  a[0]=0b10101010;
+   a[0]=0b10101010;
+   a[1]=0b10101010;
    reg.shift(a);
    a[0]=1;
+   a[1]=1;
    delay(500);
    for(int i=0;i<8;i++){
     reg.shift(a);
     a[0]=a[0]<<1;
+    a[1]=a[1]<<1;
     delay(250);
   }
   reg.shift(a);
